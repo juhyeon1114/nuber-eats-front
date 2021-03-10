4f05c348a1d6e4c6ffb418a7dfb716d8d198d3d4
unknown <juhyeon1114@gmail.com>
2021-03-10 22:02:29 +0900

    log : Testing Setup

---
+++ src/components/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "../apollo";
+import { LoggedInRouter } from "../router/logged-in-router";
+import { LoggedOutRouter } from "../router/logged-out-router";
+
+export const App = () => {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return (
+    <div className="">
+      {isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />}
+    </div>
+  );
+};
