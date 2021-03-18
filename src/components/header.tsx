fd89271757ad94881dd232507a42894859174261
unknown <juhyeon1114@gmail.com>
2021-03-18 17:52:56 +0900

    log : logout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useMe } from "../hooks/useMe";
 import nuberLogo from "../images/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Header: React.FC = () => {
+  const history = useHistory();
   const { data } = useMe();
+  const logout = () => {
+    // localStorage.removeItem("token");
+    localStorage.clear();
+    history.push("/");
+    window.location.reload();
+  };
 
   return (
     <>
@@ -15,7 +22,7 @@ export const Header: React.FC = () => {
           <span>이메일을 인증해주세요</span>
         </div>
       )}
-      <header className=" py-4">
+      <header className="py-4">
         <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
           <Link to="/">
             <img src={nuberLogo} className="w-34" alt="logo" />
@@ -25,6 +32,14 @@ export const Header: React.FC = () => {
               <FontAwesomeIcon icon={faUser} className="text-lg mr-4" />
               {data?.me.email}
             </Link>
+            &nbsp;&nbsp;
+            <FontAwesomeIcon
+              icon={faSignOutAlt}
+              className="text-lg mr-4"
+              onClick={() => {
+                logout();
+              }}
+            />
           </span>
         </div>
       </header>
